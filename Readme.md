a26494e444ef1a54b21f345e1bc106a9098c9284
apal5 <apal5@binghamton.edu>
2024-04-28 16:16:41 -0400

    Boilter plate http server

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Add module to go.mod
+```
+go get github.com/joho/godotenv
+```
+
+Add code to vendor directory
+```
+go mod vendor
+```
---
+++ json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v",payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+type errResponse struct{
+	Error string `json:"error"`
+}
+
+func responseWithError(w http.ResponseWriter, code int , msg string){
+	if code > 499{
+		log.Println("Responding with 5XX error: ",msg)
+	}
+	errRes := errResponse{
+		Error: msg,
+	}
+	respondWithJSON(w, code, errRes)
+}
